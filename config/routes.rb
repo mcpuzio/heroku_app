fcacbc851d2cdc53c0407abf90dda0fec3536486
Mike Puzio <mcpuzio@gmail.com>
2016-02-17 11:40:16 -0500

    Added scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :todos
+  root 'todos#index'
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
